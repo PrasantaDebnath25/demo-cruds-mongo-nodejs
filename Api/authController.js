cb60d72f307773913c1c07ef130d87b0d1ef4ff3
Prasanta-Debnath <prasanta@technoexponent.com>
2023-05-25 19:19:09 +0530

    master: Auth and unauth Controller added

---
+++ Api/authController.js
@@ -0,0 +1,104 @@
+import UserModel from "../Models/userModel"
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import verifyToken from "../Utils/VerifyJWTToken";
+
+exports.fetchUser = async (req, res) => {
+    // let response = await verifyToken();
+    // console.log("req---", req.user)
+    // http://localhost:8000/api/fetch-user?id=64524f885bb6bb7fba1fa673
+    // let findUser = await UserModel.findOne({ email: 1, password: 1 })
+    // console.log(findUser)
+    const user = req.user
+    let data = {
+        status: 200,
+        message: 'Fetched successfully',
+        data: {
+            user: user
+        }
+    }
+    return res.status(200).send(data)
+
+}
+
+exports.deleteUser = async (req, res) => {
+    console.log("Query---", req.query)
+    console.log("user---", req.user)
+    // http://localhost:8000/delete-user?id=645242602236689a298dac75
+    try {
+        let findUser = await UserModel.deleteOne({ _id: mongoose.Types.ObjectId(req.query.id) })
+        console.log(findUser)
+        if (!findUser) {
+            let data = {
+                status: 400,
+                message: 'User not found'
+            }
+            return res.status(400).send(data)
+        }
+
+        let data = {
+            status: 200,
+            message: 'User Deleted successfully'
+        }
+        return res.status(200).send(data)
+    } catch (err) {
+        // console.log(err)
+        let resBody = {
+            status: 400,
+            data: findUser,
+            messsage: "Error"
+        }
+        return res.status(400).send(resBody)
+    }
+}
+
+exports.deleteAllUser = async (req, res) => {
+    console.log(req.query)
+    // http://localhost:8000/delete-all-user
+    try {
+        let findUser = await UserModel.deleteMany()
+        console.log(findUser)
+        let resBody = {
+            status: 200,
+            data: findUser,
+            messsage: "Delete all user"
+        }
+        return res.send(resBody)
+
+    } catch (err) {
+        // console.log(err)
+        let resBody = {
+            status: 400,
+            data: findUser,
+            messsage: "Error"
+        }
+        return res.send(resBody)
+    }
+}
+
+exports.userList = async (req, res) => {
+    // http://localhost:8000/list
+    try {
+        let findAllUser = await UserModel.find() //All Columns
+        // let findAllUser = await UserModel.find({},{ user: 1, password: 1  })//specific Columns
+        let resBody = {
+            status: 200,
+            data: findAllUser,
+            messsage: "All users list",
+            count: findAllUser.length
+        }
+        return res.send(resBody)
+
+    } catch (err) {
+        // console.log(err)
+        let resBody = {
+            status: 400,
+            data: findAllUser,
+            messsage: "Error",
+            count: 0
+        }
+        return res.send(resBody)
+    }
+
+
+}
---
+++ Api/unAuthController.js
@@ -0,0 +1,62 @@
+import UserModel from "../Models/userModel"
+import jwt from 'jsonwebtoken';
+import config from '../config';
+
+const register = async (req, res) => {
+    console.log(req.body)
+    // http://localhost:8000/register
+    // {
+    //     "email": "pro@yopmail.com",
+    //     "name":"Pro",
+    //     "pass":"Pro@1"
+    // }
+    let users = await UserModel.create({ email: req.body.email, name: req.body.name, password: req.body.pass })
+    let resBody = {
+        status: 200,
+        data: users,
+        messsage: "User created successfully"
+    }
+    return res.status(200).send(resBody)
+
+}
+
+const login = async (req, res) => {
+    console.log(req.body)
+    // http://localhost:8000/login
+    const user = await UserModel.findOne({ $and: [{ email: req.body.email, password: req.body.pass }] })
+
+    if (!user) {
+        return res.status(200).send({
+            status: 400,
+            message: 'Credential mismatched',
+            data: {
+                _id: null,
+                accessToken: null,
+                email: req.body.email,
+                user: ""
+            }
+        });
+    }
+    console.log("User---", user)
+    const token = jwt.sign({ _id: user._id }, config.SECRET_KEY);
+    console.log("token---", token)
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    });
+
+    return res.status(200).send({
+        status: 200,
+        message: 'Logged in successfully',
+        data: {
+            _id: user._id,
+            accessToken: token,
+            email: user.email,
+            user: user
+        }
+    });
+    // return res.send('Added')
+}
+
+export default {register, login}
