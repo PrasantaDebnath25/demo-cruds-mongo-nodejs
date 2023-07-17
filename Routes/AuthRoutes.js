a3c271b4875b233bec0c51d4ef70518f07790221
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-07-17 18:46:54 +0530

    Space added

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -18,6 +18,7 @@ export default function AuthRoute(app) {
 
     // app.use(verifyToken);
 
+    
     //Auth Controller
     auth_api_router.get('/fetch-user', AuthController.fetchUser);
     auth_api_router.get('/delete-user', AuthController.deleteUser);
