e842ceb5378dbc406398c39d0a5f692e6aef07a8
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-08-08 13:09:50 +0530

    text

--- Models/userModel.js
+++ Models/userModel.js
@@ -23,5 +23,5 @@ const user = new mongoose.Schema({ //Schema variable user
 
 const UserModel = mongoose.model('User', user); //Collectionanasme -> 'User'
 
-// module.exports = UserModel; //ModelName for previous version
+// module.exports = UserModel; //ModelName for previous 
 export default UserModel;
